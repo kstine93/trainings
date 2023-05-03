7eb0670d33b33315652b1d749eb49277cec3f3e9
Kevin Stine <kevin.stine@zalando.de>
2023-05-03 20:34:16 +0200

    removed old code for setting up postgres table for dbt practice

--- dbt/dbt_fundamentals_course/initial_table_setup.py
+++ dbt/dbt_fundamentals_course/initial_table_setup.py
@@ -2,9 +2,6 @@ import psycopg2
 from psycopg2 import sql
 import csv
 
-#find postgres container ID:
-postgres_docker_container = '70cd5e4b4530'
-
 #connecting to local postgres instance:
 conn_dict = {
     'host':'localhost',
@@ -64,49 +61,4 @@ else:
             #print(query.as_string(db_conn))
             db_cur.execute(query,line)
 
-
-    # file = open('./house_prices.csv')
-    # contents = file.read()
-    # contents_arr = contents.splitlines()
-    # db_cur.execute(f"INSERT INTO house_prices VALUES ({contents_arr[1]})")
-    
-    # # Converting string to dict:
-    # dict_arr = [json.loads(val) for val in contents_arr]
-
-    # # filter by NextSong action
-    # next_song_logs = [log for log in dict_arr if log['page'] == "NextSong"]
-
-    # # convert timestamp column to datetime
-    # for log in next_song_logs:
-    #     log['ts'] = datetime.datetime.fromtimestamp(log['ts']/1000) #Converting from ms
-    
-    # insert_into_time(cur,next_song_logs)
-    # insert_into_users(cur,next_song_logs)
-    # insert_into_songplays(cur,next_song_logs)
-
-
-
-
-    # copy_command = "\copy fifa_winners FROM \'/home/rambino/dev/trainings/dbt/dbt_fundamentals_course/FIFA_award_winners.csv\' delimiter \',\' csv header"
-    # print(f"docker exec -it {postgres_docker_container} psql -U postgres dbt_practice -c '{copy_command}'")
-    # os.system(f"docker exec -it {postgres_docker_container} psql -U postgres dbt_practice -c '{copy_command}'")
-    # db_cur.execute('''
-    #     COPY fifa_winners(
-    #         key_id
-    #         ,tourn_id
-    #         ,tourn_name
-    #         ,award_id
-    #         ,award_name
-    #         ,shared
-    #         ,player_id
-    #         ,l_name
-    #         ,f_name
-    #         ,team_id
-    #         ,team_name
-    #         ,team_code
-    #     )
-    #     FROM './FIFA_award_winners.csv'
-    #     DELIMITER ','
-    #     CSV HEADER;
-    # ''')
     db_conn.commit()
